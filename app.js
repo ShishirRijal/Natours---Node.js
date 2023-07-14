f8bd4baa296f294c2a5ddd9ff97202724c2e74d9
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-15 00:45:17 +0545

    added API requests for the users

--- app.js
+++ app.js
@@ -85,17 +85,46 @@ const deleteTour =  (req, res) => {
      });
  }
 
-
+    const getAllUsers = (req, res) => { 
+         res.status(500).json({ // 500 means internal server error
+                status: 'error',
+                message: 'This route is not yet defined'
+            });
+    }
+    const createUser = (req, res) => { 
+        res.status(500).json({  
+                status: 'error',
+                message: 'This route is not yet defined'
+            });
+        }
+        const deleteUser = (req, res) => { 
+            res.status(500).json({  
+                    status: 'error',
+                    message: 'This route is not yet defined'
+                });
+            }
+            const updateUser = (req, res) => { 
+                res.status(500).json({  
+                        status: 'error',
+                        message: 'This route is not yet defined'
+                    });
+                }
+                const getUser = (req, res) => { 
+                    res.status(500).json({  
+                            status: 'error',
+                            message: 'This route is not yet defined'
+                        });
+                    }
+            
+        
+    
  // * ROUTES
-// app.get('/api/v1/tours',getAllTour);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour)
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTour).post(createTour);
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
  // SERVER 
  const port = process.env.port || 8000;
