9f9d181360ed9d169ce5fa888a0034ebe732425c
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-15 02:04:48 +0545

    added dotenv dependency to work with environment variables

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env '});
+
 const app = require('./app');
- 
+
+
+console.log(process.env);
+
 const port = process.env.port || 8000;
 app.listen(port, (req, res) => {
        console.log(`Server is running on port ${port}`);
