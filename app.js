cbf26663050e0498c349c8678b48e3e820255747
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-15 01:46:25 +0545

    added middleware to access static files

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const tourRouter = require(`${__dirname}/routes/tour-routes.js`);
 const userRouter = require(`${__dirname}/routes/user-routes.js`);
 
  const app = express(); 
+
  
 //*  MIDDLEWARES -- called so because it sits between the request and the response
 // it can modify the incoming request data
 app.use(express.json());  // the data from the body is added to the request object
 app.use(morgan('dev')); //  morgan is a logging middleware that logs the request data to the console
-
+app.use(express.static(`${__dirname}/public`)); // access the static files
     
 // * ROUTES
 // mount the router 
