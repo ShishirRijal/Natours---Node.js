42082330c736f1f82784693f73ff0b972fb1177c
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-15 14:47:41 +0545

    updated the getTour api to fetch particular tour by id from database

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -37,20 +37,21 @@ exports.createTour = async  (req, res) => {
         })
     }
 }
-exports.getTour =  (req, res) => {
-    const id = req.params.id * 1; // convert id from string to number
-    console.log(typeof(id));
-    const tour  = tours.find((tour) => tour.id === id);
-    if(!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID' 
-        })
-    } 
-    res.status(200).json({ 
+exports.getTour = async (req, res) => {
+    try {
+        console.log(req.params.id);  
+        const tour = await Tour.findById(req.params.id); // req.params.id is the id in the url
+        res.status(200).json({ 
         status: 'success',
         data: { tour } 
-    });
+        });
+    }
+    catch(err) {
+        res.status(404).json({
+            status: 'failure',
+            message: err
+        })
+    }
 }
 
 exports.updateTour =  (req, res) => { 
