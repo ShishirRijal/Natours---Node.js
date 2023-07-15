1adbcc2876f8226784f28fa912483e23e7ef0886
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-15 12:47:56 +0545

    created tour schema and defined a tour model out of it

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ mongoose.connect(DB, {
        useNewUrlParser: true,
 }).then(conn => console.log("Database connected successfully"));
 
+
+
+const tourSchema = new mongoose.Schema({
+       name: {
+              type: String,
+              required: [true, 'A tour must have a name'],
+              unique: [true, 'A tour already exists by this name']
+       }, 
+       rating: {
+              type: Number,
+              default: 4.5
+       }, 
+       price: {
+              type: Number,
+              required: [true, 'A tour must have a price']
+       }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+
 const port = process.env.port || 8000;
 app.listen(port, (req, res) => {
        console.log(`Server is running on port ${port}`);
