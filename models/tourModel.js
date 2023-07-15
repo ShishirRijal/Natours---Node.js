35e8309fd1b724caba47a9e2d98075d1881e5a92
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-15 16:07:39 +0545

    added script to import and delete the collections in database

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,19 +1,59 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-    name: {
-           type: String,
-           required:  [true, 'A tour must have a name'],
-           unique: [true, 'A tour already exists by this name']
-    }, 
-    rating: {
-           type: Number,
-           default: 4.5
-    }, 
-    price: {
-           type: Number,
-           required: [true, 'A tour must have a price']
-    }
+       name: {
+              type: String,
+              required:  [true, 'A tour must have a name'],
+              unique: [true, 'A tour already exists by this name']
+       }, 
+       duration: {
+              type: Number, 
+              required: [true, 'A tour must have a duration']
+       }, 
+       maxGroupSize: {
+              type: Number, 
+              required: [true, 'A tour must have a group size']
+       },
+       difficulty: {
+              type: String,
+              required: [true, 'A tour must have a difficulty']
+       }, 
+       ratingsAverage: {
+              type: Number,
+              default: 4.5
+       }, 
+       ratingsQuantity: { 
+              type: Number,
+              default: 0
+       },
+       price: {
+              type: Number,
+              required: [true, 'A tour must have a price']
+       }, 
+       priceDiscount: { 
+              type: Number,
+              default: 0
+       },
+       summary: {
+                     type: String,
+                     trim: true,
+                     required: [true, 'A tour must have a summary']
+       }, 
+       description: {
+              type: String,
+              trim: true,
+              required: [true, 'A tour must have a description']
+       }, 
+       imageCover: {
+              type: String,
+              required: [true, 'A tour must have a cover image']
+       },
+       images: [String],
+       createdAt: {
+              type: Date,
+              default: Date.now()
+       },
+       startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
