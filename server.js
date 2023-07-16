1e1bc93e1e27af7859d671da7b06ea6099361ab8
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-17 01:02:28 +0545

    handled the uncaught exceptions

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
  const dotenv = require('dotenv').config();
+
+
+//* Handling uncaught exceptions
+// All errors that occur in synchronous code and but not handled 
+// are called uncaught exceptions
+process.on('uncaughtException', err => {
+       console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+       console.log(err.name, err.message);
+       // In such case, we need to close the server first and then exit the process
+       server.close(() => {
+              process.exit(1);
+       });
+});
+
+
 const app = require('./app');
 const mongoose = require('mongoose');
 
@@ -20,7 +35,7 @@ const server = app.listen(port, (req, res) => {
 }) 
 
 
-// Handling uncaught exceptions
+// Handling the unhandled rejections
 process.on('unhandledRejection', err => {
        console.log('UNHANDLED REJECTION! 💥 Shutting down...');
        console.log(err.name, err.message); 
@@ -29,3 +44,6 @@ process.on('unhandledRejection', err => {
               process.exit(1);
        });
 });
+
+
+
