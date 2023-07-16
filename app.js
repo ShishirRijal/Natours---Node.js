b6872092317b57045883a391b873ff7866189b93
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-16 18:58:58 +0545

    added a global error handling midleware

--- app.js
+++ app.js
@@ -22,6 +22,32 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 
+// if we make upto here, this is an undefined route
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'failure', 
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.statusCode = 404;
+    err.status = 'Failure';
+    // if we pass anything to the next function, express will assume that it is an error and will skip all the middlewares and go straight to the error handling middleware
+    next(err); // passing the error to the next middleware
+})
+
+
+// Error handling middleware
+// giving 4 parameters to the middleware tells express that this is an error handling middleware
+app.use((err, req, res, next) => {
+     err.statusCode = err.statusCode || 500; 
+     err.status = err.status || 'error';
+     res.status(err.statusCode).json({
+        status: err.status, 
+        message: err.message,
+     })
+
+})
+
 
 
 module.exports = app;
