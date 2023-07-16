db734be7ec30711c652fa7cd6b82b7a36adc79dc
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-16 22:17:10 +0545

    added a custom error class and refactored the code using this class

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan'); 
+const AppError = require(`${__dirname}/utils/app-error.js`);
+const globalErrorHandler = require(`${__dirname}/controllers/error-controller.js`);
 
 // import routes
 const tourRouter = require(`${__dirname}/routes/tour-routes.js`); 
@@ -24,29 +26,21 @@ app.use('/api/v1/users', userRouter);
 
 // if we make upto here, this is an undefined route
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'failure', 
-    //     message: `Can't find ${req.originalUrl} on this server`
-    // });
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.statusCode = 404;
-    err.status = 'Failure';
+    // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    // err.statusCode = 404;
+    // err.status = 'Failure'; 
     // if we pass anything to the next function, express will assume that it is an error and will skip all the middlewares and go straight to the error handling middleware
-    next(err); // passing the error to the next middleware
+    // next(err); // passing the error to the next middleware
+
+    // using custom error class, we can do this
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+
 })
 
 
 // Error handling middleware
 // giving 4 parameters to the middleware tells express that this is an error handling middleware
-app.use((err, req, res, next) => {
-     err.statusCode = err.statusCode || 500; 
-     err.status = err.status || 'error';
-     res.status(err.statusCode).json({
-        status: err.status, 
-        message: err.message,
-     })
-
-})
+app.use(globalErrorHandler); 
 
 
 
---
+++ utils/app-error.js
@@ -0,0 +1,13 @@
+class AppError extends Error {
+    constructor(message, statusCode) { 
+        super(message); // this will call the constructor of the parent class
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'failure' : 'error';
+        this.isOperational = true; // this will be used to check if the error is operational or not
+       
+       
+        Error.captureStackTrace(this, this.constructor); // this will not include this constructor in the stack trace
+    }
+}
+
+module.exports = AppError;
