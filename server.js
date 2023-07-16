31c229bf580f8dca5701de126f5b2a6684c65b6e
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-17 00:51:34 +0545

    handled unhandled rejected promises

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB, { 
        useNewUrlParser: true,
-}).then(() => console.log('DB connection successful!')).catch(err => console.log(err));
+}).then(() => console.log('DB connection successful!'));
 
 
 
@@ -15,6 +15,17 @@ mongoose.connect(DB, {
 
 //* SERVER
 const port = process.env.PORT || 8000;
-app.listen(port, (req, res) => {
+const server = app.listen(port, (req, res) => {
        console.log(`Server is running on port ${port}`);
 }) 
+
+
+// Handling uncaught exceptions
+process.on('unhandledRejection', err => {
+       console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+       console.log(err.name, err.message); 
+       // In such case, we need to close the server first and then exit the process
+       server.close(() => {
+              process.exit(1);
+       });
+});
