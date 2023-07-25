171164ff5b18efe92052822bc7f2d9662d7c33b7
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-25 23:40:53 +0545

    added get all users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,16 @@
-exports.getAllUsers = (req, res) => { 
-    res.status(500).json({ // 500 means internal server error
-        status: 'error',
-        message: 'This route is not yet defined'
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catch-async');
+
+
+exports.getAllUsers = catchAsync(async (req, res, next) => { 
+    const users = await User.find();
+
+    res.status(200).json({
+       "status": "success", 
+       "results": users.length,
+       "data": { users }
        });
-}
+});
 exports.createUser = (req, res) => { 
     res.status(500).json({ // 500 means internal server error
         status: 'error',
